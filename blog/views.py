84d4149e2c5d628fe1b7526ebc42eeba8910f431
nafiseh <nh.edris@gmail.com>
2023-10-31 19:31:14 +0330

    timezone
    import timezone

--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,17 @@
 from django.shortcuts import render,get_object_or_404
 from blog.models import post
-import datetime
-
+from django.utils import timezone
 
 
 def blog_view(request):
-   # start_date = datetime.date(2000, 1, 1)
-    #end_date = datetime.datetime.now()
-   # posts=post.objects.filter(published_date__range=(start_date, end_date)) 
-    posts= post.objects.filter(published_date__lte=datetime.datetime.now(),status=1)
+   
+    posts= post.objects.filter(published_date__lte=timezone.now(),status=1)
     context={'posts':posts} 
     return render (request,'blog/blog-home.html',context)
       
               
 def blog_single(request,pid):
+
     posts=get_object_or_404(post,pk=pid)
     if post:
         posts.counted_views = posts.counted_views + 1
