65af1ab2a134342c31f2daa8629181f9bd48a409
nafiseh <nh.edris@gmail.com>
2023-10-31 21:23:15 +0330

    Add author field and category field
    Add author field and category field

--- blog/admin.py
+++ blog/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from blog.models import post 
+from blog.models import post,category 
 
 class postAdmin(admin.ModelAdmin):
-    list_display=['title','counted_views','status','published_date','created_date']
-    list_filter=('status',)
+    list_display=['title','author','counted_views','status','published_date','created_date']
+    list_filter=('status','author',)
     search_fields=['title','content']
     empty_value_display = '-empty-'
     date_hierarchy ='created_date'
     
 admin.site.register(post,postAdmin)
+admin.site.register(category)
--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-
+class category(models.Model):
+    name=models.CharField(max_length=255)
+        
+    def __str__(self):
+          return self.name
+      
 
 class post (models.Model):
     title= models.CharField(max_length=255)
+    author=models.ForeignKey(User,on_delete=models.CASCADE,null=True)
     content= models.TextField()
+    image=models.ImageField(upload_to='blog/')
+    category=models.ManyToManyField(category)
     counted_views = models.IntegerField(default=0)
     status=models.BooleanField(default=False)
     created_date=models.DateTimeField(auto_now_add=True)
     updated_date=models.DateTimeField(auto_now=True)
     published_date=models.DateTimeField( null=True)
+    
     
 class Meta:
          ordering=['created_date']
@@ -17,5 +27,3 @@ class Meta:
 def __str__(self):
           return self.title
       
-    
-
